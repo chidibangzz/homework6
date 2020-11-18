43873a358a8e9bda431779459d7c93fcf4832c7d
Chidi Ekeke <chidiekeke18@yahoo.com>
2020-11-18 12:04:16 -0600

    api's

---
+++ weather.js
@@ -0,0 +1,17 @@
+//weather api
+"http://api.openweathermap.org/data/2.5/weather"
+
+queryURL = "http://api.openweathermap.org/data/2.5/weather?q=austin&appid=9eb51aba9e5654b6e1bc5b9934efaba8&units=imperial"
+
+//forecast api
+ "http://api.openweathermap.org/data/2.5/forecast"
+
+
+queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=austin&appid=9eb51aba9e5654b6e1bc5b9934efaba8&units=imperial"
+
+//data api
+
+"http://api.openweathermap.org/data/2.5/uvi"
+
+
+queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=9eb51aba9e5654b6e1bc5b9934efaba8&lat=30.27&lon=-97.74"
